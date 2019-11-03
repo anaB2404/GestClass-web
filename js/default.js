a4388db9cb65addfcc25de9b75cde46be9a9aba4
Ana Beatriz Rodrigues <anaana.abl@gmail.com>
2019-11-03 12:21:50 -0300

    editando o floating-action-button, terminando de editar o menu

--- js/default.js
+++ js/default.js
@@ -8,4 +8,14 @@ document.addEventListener('DOMContentLoaded', function() {
 $(document).ready(function(){
   $('.fixed-action-btn').floatingActionButton();
 });
-   
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.tooltipped');
+  var instances = M.Tooltip.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.tooltipped').tooltip();
+});
