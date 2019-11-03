1881c1052bbf5b1318ff88479124cddacb33335c
Ana Beatriz Rodrigues <anaana.abl@gmail.com>
2019-11-02 21:03:05 -0300

    adicionando a pagina de manutenção, adicionando floating action button, editando a pagina da secretaria e adcionando dropdown no menu sidenav

--- js/homeSecretaria.js
+++ js/homeSecretaria.js
@@ -8,3 +8,12 @@ document.addEventListener('DOMContentLoaded', function() {
 $(document).ready(function(){
   $('.sidenav').sidenav();
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, options);
+});
+
+// Or with jQuery
+
+$('.dropdown-trigger').dropdown();
