de5c133bf9778ae6ca23bdb7f0bb64988abe44e3
CAIO FONSECA GONCALVES <59305538X@alunos.etec.itaqua>
2019-11-27 18:15:41 -0300

    Adicionando cadastro das contas na secretaria

--- js/homeSecretaria.js
+++ js/homeSecretaria.js
@@ -1,14 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-});
-
-
-
 $(document).ready(function(){
   $('.sidenav').sidenav();
 });
 
-// Or with jQuery
-
 $('.dropdown-trigger').dropdown();
+
+$(document).ready(function(){
+  $('.modal').modal();
+});
+
+$(document).ready(function(){
+  $('select').formSelect();
+});
+
+$(".formContas").hide();
+
+function habilitaForm(){
+  //alert($("#selectConta")[0].selectedIndex)
+
+  $(".formContas").hide();
+
+  if($("#selectConta")[0].selectedIndex === 1){
+    $('#aluno').show(500)
+  } else if ($("#selectConta")[0].selectedIndex === 2) {
+    $('#responsavel').show(500)
+  } else if ($("#selectConta")[0].selectedIndex === 3) {
+    $('#professor').show(500)
+  } else{
+    $('#secretria').show(500)
+  }
+}
