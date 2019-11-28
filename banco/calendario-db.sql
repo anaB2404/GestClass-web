f3dc336a3c9fd7d9a675f1477bed50c9dbb1802a
Apresentação <apresentacao@alunos.etec.itaqua>
2019-11-28 18:09:49 -0300

    subindo banco calendario

--- banco/calendario-db.sql
+++ banco/calendario-db.sql
@@ -1,26 +1,26 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
+-- version 4.0.4.2
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1:3306
--- Generation Time: 28-Nov-2019 às 12:06
--- Versão do servidor: 5.7.26
--- versão do PHP: 5.6.40
+-- Máquina: localhost
+-- Data de Criação: 28-Nov-2019 às 21:09
+-- Versão do servidor: 5.6.13
+-- versão do PHP: 5.4.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Database: `calendario-db`
+-- Base de Dados: `calendario-db`
 --
+CREATE DATABASE IF NOT EXISTS `calendario-db` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `calendario-db`;
 
 -- --------------------------------------------------------
 
@@ -28,7 +28,6 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `events`
 --
 
-DROP TABLE IF EXISTS `events`;
 CREATE TABLE IF NOT EXISTS `events` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
@@ -36,16 +35,21 @@ CREATE TABLE IF NOT EXISTS `events` (
   `start` datetime DEFAULT NULL,
   `end` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Extraindo dados da tabela `events`
 --
 
 INSERT INTO `events` (`id`, `title`, `color`, `start`, `end`) VALUES
-(1, 'IONIC', '#42a5f5', '2019-11-18 19:00:00', '2019-11-18 22:30:00'),
-(2, 'Banco de dados', '#b71c1c', '2019-11-19 00:00:00', '2019-11-19 20:50:00');
-COMMIT;
+(1, 'IONIC', '#42A5F5', '2019-11-18 19:00:00', '2019-11-18 22:30:00'),
+(2, 'Banco de dados ', '#4a148c', '2019-11-19 19:00:00', '2019-11-19 20:50:00'),
+(3, 'Ingles', '#00695c', '2019-11-19 21:05:00', '2019-11-19 22:30:00'),
+(4, 'Programacao WEB', '#00b8d4', '2019-11-20 19:00:00', '2019-11-20 22:30:00'),
+(5, 'Planejamento de Trabalho de Conclusao de Curso', '#bf360c', '2019-11-21 19:00:00', '2019-11-21 20:50:00'),
+(6, 'Internet e Protocolos', '#3e2723', '2019-11-21 21:05:00', '2019-11-21 22:30:00'),
+(7, 'Desenvolvimento de sistemas', '#b71c1d', '2019-11-22 19:00:00', '2019-11-22 22:30:00'),
+(8, 'Semana TCC e Trabalhos interdiciplinares', '#424242', '2019-11-25 19:30:00', '2019-11-29 22:30:00');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
