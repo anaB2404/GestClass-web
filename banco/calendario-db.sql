3ca819f1abfcfec5ed8c7dcc30d3ea77385132fa
Ana Beatriz Rodrigues Lopes <anaana.abl@gmail.com>
2019-11-28 09:07:41 -0300

    Subindo o banco com alguns dados

--- banco/calendario-db.sql
+++ banco/calendario-db.sql
@@ -1,26 +1,26 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.2
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
--- Máquina: localhost
--- Data de Criação: 19-Nov-2019 às 01:01
--- Versão do servidor: 5.6.13
--- versão do PHP: 5.4.17
+-- Host: 127.0.0.1:3306
+-- Generation Time: 28-Nov-2019 às 12:06
+-- Versão do servidor: 5.7.26
+-- versão do PHP: 5.6.40
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de Dados: `calendario-db`
+-- Database: `calendario-db`
 --
-CREATE DATABASE IF NOT EXISTS `calendario-db` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `calendario-db`;
 
 -- --------------------------------------------------------
 
@@ -28,6 +28,7 @@ USE `calendario-db`;
 -- Estrutura da tabela `events`
 --
 
+DROP TABLE IF EXISTS `events`;
 CREATE TABLE IF NOT EXISTS `events` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
@@ -35,16 +36,16 @@ CREATE TABLE IF NOT EXISTS `events` (
   `start` datetime DEFAULT NULL,
   `end` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `events`
 --
 
 INSERT INTO `events` (`id`, `title`, `color`, `start`, `end`) VALUES
-(1, 'AULA IONIC', '#42a5f5', '2019-11-18 19:00:00', '2019-11-13 22:00:00'),
-(2, 'Aula Banco de dados', '#ad1457', '2019-11-19 19:00:00', '2019-11-19 20:50:00'),
-(3, 'Aula de Ingles', '#4a148c', '2019-11-19 21:05:00', '2019-11-19 22:30:00');
+(1, 'IONIC', '#42a5f5', '2019-11-18 19:00:00', '2019-11-18 22:30:00'),
+(2, 'Banco de dados', '#b71c1c', '2019-11-19 00:00:00', '2019-11-19 20:50:00');
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
